97d2d58f495d7a3ffd7b34e0fbd8532db41dd0ca
Sky <sky@Skys-MacBook-Pro.local>
2018-07-25 11:28:28 -0700

    finished the lab

---
+++ solution.js
@@ -0,0 +1,49 @@
+var beeps = 2;
+var boops = 6;
+var total = beeps + boops;
+console.log(total);
+
+beeps = 0;
+boops = 5;
+total = beeps + boops;
+console.log(total);
+
+beeps = 9;
+boops = 3;
+total = beeps + boops;
+console.log(total);
+
+beeps = 4;
+boops = 8;
+total = beeps + boops;
+console.log(total);
+
+beeps = 10;
+boops = 5;
+total = beeps + boops;
+console.log(total);
+
+beeps = 11;
+boops = 12;
+total = beeps + boops;
+console.log(total);
+
+beeps = 5;
+boops = 5;
+total = beeps + boops;
+console.log(total);
+
+beeps = 1;
+boops = 17;
+total = beeps + boops;
+console.log(total);
+
+beeps = 5;
+boops = 7;
+total = beeps + boops;
+console.log(total);
+
+beeps = 4;
+boops = 0;
+total = beeps + boops;
+console.log(total);
