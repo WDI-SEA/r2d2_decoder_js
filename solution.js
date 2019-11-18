9751a270300f5e552d9ec9a7866a54a9e5740adf
RebeccaEvans <evans.rebeccaj@gmail.com>
2019-11-18 14:15:36 -0800

    rebecca updated for bonus

--- solution.js
+++ solution.js
@@ -32,14 +32,28 @@ let decoderTable = {
 // These are the inputs that you have
 let inputs = [[2, 6], [0, 5], [9, 3], [4, 8], [10, 5], 'BOP', [11, 12], [5, 10], [1, 17], [5, 7], [4, 0]];
 
+// Bonus 1
+
+let bonus1 = [[1, 8], 'BOP', [5, 7], [13, 2], [9, 13], [1, 4], 'BOP', [2, 1], [0, 1], [10, 10], [10, 9]];
+
+// Bonus 2
+
+let bonus2 = [[9, 16], [13, 2], [13, 8], 'BOP', [2, 1], [0, 1], [10, 4], 'BOP', [2, 2],  [4, 11], 'BOP',  [4, 9], [8, 7], [7, 11], [3, 2], 'BOP', [11, 9], [3, 5], [0, 1], [7, 7], 'BOP', [13, 12], [13, 2], [8, 13], 'BOP', [5, 6], [12, 2], [9, 6], [20, 3]];
+
+// Bonus 3
+
+let bonus3 = [[1, 0], 'BOP', [2, 1], [3, 5], [0, 1], [4, 9], [6, 10], [3, 6],  [4, 11], [8, 6], 'BOP', [2, 7], [12, 7], 'BOP', [3, 1], [5, 0], [1, 5], [4, 5], [12, 2], [2, 3], [1, 3], 'BOP', [9, 5], [12, 3], [14, 6], 'BOP', [1, 1], [22, 3], 'BOP', [8, 12], [4, 4], [4, 1], [8, 1], [11, 7], 'BOP', [12, 11], [3, 6], [8, 6], [12, 7], 'BOP', [0, 2], [10, 11], [11, 9], 'BOP', [2, 0], [25, 0], 'BOP', [3, 5], [10, 5], [10, 13], 'BOP', [11, 9], [3, 5], [4, 1], [17, 8], 'BOP', [2, 1], [1, 0], [5, 9], 'BOP', [16, 2], [1, 4], [3, 0], [7, 8], [9, 13], [2, 3], [9, 9], 'BOP', [12, 11], [4, 4], [5, 0], [8, 6], 'BOP', [11, 9], [3, 5], [4, 1], [17, 8], 'BOP', [0, 6], [1, 0], [9, 3], [0, 12]];
+
+
 var letters = []
 
-for (i=0; i<inputs.length; i++) {
-  if (inputs[i] === 'BOP') {
+const solveThis = (chooseInput) => {
+for (i=0; i<chooseInput.length; i++) {
+  if (chooseInput[i] === 'BOP') {
     letters.push('BOP')
   }
   else {
-    letters.push(inputs[i][0] + inputs[i][1])
+    letters.push(chooseInput[i][0] + chooseInput[i][1])
     }
 }
 
@@ -48,12 +62,18 @@ console.log(letters)
 for (i=0; i<letters.length; i++) {
   console.log(decoderTable[letters[i]])
 }
+}
+
+
+solveThis(bonus1)
+solveThis(bonus2)
+solveThis(bonus3)
 
 
 // STEP 1: Make a new, empty array called letters.
 
 // STEP 2: Add the total number of beeps + boops to the letters array. This will
-// include looping over the inputs array to get the totals.
+// include looping over the chooseInput array to get the totals.
 
 // In other words, for each array inside the inputs array (starting with [2, 6]),
 // the left number (2) is the number of beeps and the right number (6) is the
